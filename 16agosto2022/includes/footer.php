210de2a456f7de673f0a8ef62abefac8febb204a
ferg0311 <69618092+ferg0311@users.noreply.github.com>
2022-08-16 18:23:30 -0600

    Add files via upload

---
+++ 16agosto2022/includes/footer.php
@@ -0,0 +1,5 @@
+<body>
+<footer>
+<h3>Fin de clase</h3>
+</footer>
+</body>
---
+++ 16agosto2022/prueba02.php
@@ -0,0 +1,12 @@
+<?php include "includes/header.php";?>
+
+<?php
+echo "</br>";
+$nombre="Pepe";
+echo "Hola <b> $nombre </b>, bienvenido a clase";
+echo "hola".$nombre."gusto de verte";
+
+?>
+
+<?php include "includes/footer.php";?>
+
